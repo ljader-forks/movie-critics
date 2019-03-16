b962b78b1c6e7040f1d54e2f119f5e1c77c8270b
Łukasz Ryś <it212@g2a.com>
2019-03-16 11:32:57 +0100

    Init project with rate-service

---
+++ rate-service/src/test/java/com/rys/moviecriticts/rate/RateApplicationIT.java
@@ -0,0 +1,15 @@
+package com.rys.moviecriticts.rate;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateApplicationIT {
+
+    @Test
+    @DisplayName("Init test")
+    void start() {
+
+    }
+}
